701f525a9459597a9f71680f491dbeabf84553d7
rushi2770 <rushikeshchikka@gmail.com>
2018-10-31 19:33:14 -0500

    Implemented AuthController and Register method.

---
+++ DatingAppAPI/DatingAppAPI/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAppAPI.Data;
+using DatingAppAPI.DTOs;
+using DatingAppAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDTO userForRegisterDTO)
+        {
+            
+            userForRegisterDTO.Username = userForRegisterDTO.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDTO.Username))
+                return BadRequest("User already exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDTO.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDTO.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
