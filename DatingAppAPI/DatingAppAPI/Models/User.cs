4f3fc466e87851cd835cd73494b6051e8b611543
rushi2770 <rushikeshchikka@gmail.com>
2019-01-05 10:13:39 -0600

    extended user class

--- DatingAppAPI/DatingAppAPI/Models/User.cs
+++ DatingAppAPI/DatingAppAPI/Models/User.cs
@@ -31,7 +31,7 @@ namespace DatingAppAPI.Models
 
         public string Interests { get; set; }
 
-        public string City { get; set; }
+        public string City { get; set; } 
 
         public string Country { get; set; }
 
