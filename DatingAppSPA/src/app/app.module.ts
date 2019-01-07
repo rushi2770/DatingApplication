f8bfaa439b34bd23e825098849ed19a29a56369e
rushi2770 <rushikeshchikka@gmail.com>
2019-01-07 17:19:07 -0600

    Implemented member details page and added css classes

--- DatingAppSPA/src/app/app.module.ts
+++ DatingAppSPA/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 
 export function tokenGetter() {
@@ -38,7 +39,8 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberListComponent,
-      MemberCardComponent
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
