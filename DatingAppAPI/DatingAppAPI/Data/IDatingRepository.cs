ed75d8af7565654b4deabf2c91a2db55f95adf2e
rushi2770 <rushikeshchikka@gmail.com>
2019-01-17 14:17:38 -0600

    Added LikeEntity and method to post the likes from a user

--- DatingAppAPI/DatingAppAPI/Data/IDatingRepository.cs
+++ DatingAppAPI/DatingAppAPI/Data/IDatingRepository.cs
@@ -17,5 +17,6 @@ namespace DatingAppAPI.Data
         Task<User> GetUser(int id);
         Task<Photo> GetPhoto(int id);
         Task<Photo> GetMainPhotoForUser(int userId);
+        Task<Like> GetLike(int userId, int recipentId);
     }
 }
