14755750ea5581e400fc62b8062bee8c1385633f
rushi2770 <rushikeshchikka@gmail.com>
2019-01-18 10:24:40 -0600

    Merge branch 'developer' of https://github.com/rushi2770/DatingApplication into developer

--- DatingAppAPI/DatingAppAPI/Controllers/UsersController.cs
+++ DatingAppAPI/DatingAppAPI/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using AutoMapper;
 using DatingAppAPI.Data;
 using DatingAppAPI.DTOs;
 using DatingAppAPI.Helpers;
+using DatingAppAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -73,6 +74,36 @@ namespace DatingAppAPI.Controllers
                 return NoContent();
 
             throw new Exception($"Updating user with {id} failed on save");
+        }
+
+        [HttpPost("{id}/like/{recipientId}")]
+        public async Task<IActionResult> LikeUser(int id, int recipientId)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var like = await _repo.GetLike(id, recipientId);
+
+            if (like != null)
+            {
+                return BadRequest("You already like this user");
+            }
+
+            if (await _repo.GetUser(recipientId) == null)
+                return NotFound();
+
+            like = new Like
+            {
+                LikerId = id,
+                LikeeId = recipientId
+            };
+
+            _repo.Add<Like>(like);
+
+            if (await _repo.SaveAll())
+                return Ok();
+
+            return BadRequest("Failed to like user");
         }
     }
 }
