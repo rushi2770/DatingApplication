ca04d0eba57de980c7a57e387bec2de1ca85d7a5
rushi2770 <rushikeshchikka@gmail.com>
2019-01-21 18:29:05 -0600

    returned the modified sender and recipient details.

--- DatingAppAPI/DatingAppAPI/Controllers/MessagesController.cs
+++ DatingAppAPI/DatingAppAPI/Controllers/MessagesController.cs
@@ -76,7 +76,9 @@ namespace DatingAppAPI.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDTO messageForCreationDTO)
         {
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            var sender = await _repo.GetUser(userId);
+
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
 
             messageForCreationDTO.SenderId = userId;
@@ -90,10 +92,12 @@ namespace DatingAppAPI.Controllers
 
             _repo.Add(message);
 
-            var messageToReturn = _mapper.Map<MessageForCreationDTO>(message);
-
             if (await _repo.SaveAll())
+            {
+                var messageToReturn = _mapper.Map<MessageToReturnDTO>(message);
                 return CreatedAtRoute("GetMessage", new { id = message.Id }, messageToReturn);
+            }
+               
 
             throw new Exception("Creating the message failed on save");
         }
