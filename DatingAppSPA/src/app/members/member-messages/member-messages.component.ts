63d2732e014e089120011a6cdebbf69d1a80da81
rushi2770 <rushikeshchikka@gmail.com>
2019-01-22 19:01:56 -0600

    added delete functionality

--- DatingAppSPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingAppSPA/src/app/members/member-messages/member-messages.component.ts
@@ -12,6 +12,7 @@ import { AlertifyService } from '../../_services/alertify.service';
 export class MemberMessagesComponent implements OnInit {
   @Input() recipientId : number;
   messages: Message[];
+  newMessage: any = {};
   
   constructor(private userService: UserService, private authService: AuthService,
     private alertify: AlertifyService) { }
@@ -29,4 +30,13 @@ export class MemberMessagesComponent implements OnInit {
       });
   }
 
+  sendMessage() {
+    this.newMessage.recipientId = this.recipientId;
+    this.userService.sendMessage(this.authService.decodedToken.nameid, this.newMessage).subscribe((message: Message) => {
+      this.messages.unshift(message);
+      this.newMessage.content = '';
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
 }
