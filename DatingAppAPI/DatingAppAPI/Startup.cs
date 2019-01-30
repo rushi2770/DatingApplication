b7ddce029dce6b3a74318075f2a1ceb2133525c3
rushi2770 <rushikeshchikka@gmail.com>
2019-01-30 16:46:33 -0600

    application hosted on http://datingapplication.azurewebsites.net

--- DatingAppAPI/DatingAppAPI/Startup.cs
+++ DatingAppAPI/DatingAppAPI/Startup.cs
@@ -88,7 +88,7 @@ namespace DatingAppAPI
                 });
                 //app.UseHsts();
             }
-            //seeder.SeedUsers();
+            seeder.SeedUsers();
             app.UseCors(x   => x.AllowAnyHeader()
                                 .AllowAnyOrigin()
                                 .AllowAnyMethod());
